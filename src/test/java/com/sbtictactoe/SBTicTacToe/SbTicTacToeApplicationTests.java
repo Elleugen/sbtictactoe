4b401abdbbcebd6dad1836d33fd9f0ae6f103907
Ell <farrascpi14@gmail.com>
2024-02-15 19:43:26 +0700

    COMMIT - Initial commit

---
+++ src/test/java/com/sbtictactoe/SBTicTacToe/SbTicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbtictactoe.SBTicTacToe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
