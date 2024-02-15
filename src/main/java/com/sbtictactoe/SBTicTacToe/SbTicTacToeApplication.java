2f7aa3a561fb846222a4df2e8667d98ff4057e2b
Ell <farrascpi14@gmail.com>
2024-02-15 21:32:45 +0700

    COMMIT - Create board

--- src/main/java/com/sbtictactoe/SBTicTacToe/SbTicTacToeApplication.java
+++ src/main/java/com/sbtictactoe/SBTicTacToe/SbTicTacToeApplication.java
@@ -4,12 +4,32 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.util.Scanner;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SbTicTacToeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbTicTacToeApplication.class, args);
 
-		System.out.println("\nHello world");
+		Scanner scan = new Scanner(System.in);
+		int sizeRow = 0, sizeCol = 0;
+
+		while (true) {
+			System.out.println("\nHello, this is SBTictactoe");
+			System.out.println("Customize your board to play!");
+			System.out.println("Size row: ");
+			sizeRow = scan.nextInt();
+			System.out.println("Size column: ");
+			sizeCol = scan.nextInt();
+			if(sizeRow < 10 && sizeCol < 10 && sizeRow > 2 && sizeCol > 2) {
+				Board board = new Board() ;
+				Board.field();
+				System.out.print(board.board(sizeRow,sizeCol));
+			} else {
+				System.out.println("Row or Column should be between 3-9. Try again!");
+			}
+		}
+
 	}
 }
