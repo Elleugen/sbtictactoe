e2263620288f1b85045cfb7734c51b1f7df4eab8
Ell <farrascpi14@gmail.com>
2024-02-15 19:48:19 +0700

    COMMIT - Test new version

--- src/main/java/com/sbtictactoe/SBTicTacToe/SbTicTacToeApplication.java
+++ src/main/java/com/sbtictactoe/SBTicTacToe/SbTicTacToeApplication.java
@@ -2,12 +2,14 @@ package com.sbtictactoe.SBTicTacToe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SbTicTacToeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbTicTacToeApplication.class, args);
-	}
 
+		System.out.println("\nHello world");
+	}
 }
